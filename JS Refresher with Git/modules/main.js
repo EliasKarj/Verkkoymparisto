c9e472faf1220673de29e2ff5e7b4930edd2e068
EliasKarj <145108480+EliasKarj@users.noreply.github.com>
2025-02-18 17:11:22 +0200

    Added modules for math operations and tested import/export

---
+++ JS Refresher with Git/modules/main.js
@@ -0,0 +1,7 @@
+import { add, subtract } from './math.js';
+
+const num1 = 5;
+const num2 = 3;
+
+console.log(`The sum of ${num1} and ${num2} is: ${add(num1, num2)}`);
+console.log(`The difference between ${num1} and ${num2} is: ${subtract(num1, num2)}`);
